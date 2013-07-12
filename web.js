26385e2dbaf7ddd8b174efc8ab29457dbe3103a9
Ubuntu <ubuntu@ip-172-31-24-250.us-west-2.compute.internal>
2013-07-12 23:34:34 +0000

    Buffer updates

--- web.js
+++ web.js
@@ -2,10 +2,9 @@ var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
-var buf = new Buffer(256);
 
-var contents = fs.readFileSync('index.html');
-var contents2 = buf.write(contents, 0);
+var contents = fs.readFileSync('index.html', 'utf8');
+var contents2 = contents.toString();
 
 app.get('/', function(request, response) {
   response.send(contents2);
